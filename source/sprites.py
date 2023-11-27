c89564aec346c9e281603b9d020c9720ec1adab2
sebmarl <149390512+sebmarl@users.noreply.github.com>
2023-11-27 08:52:08 -0500

    Update sprites.py

--- source/sprites.py
+++ source/sprites.py
@@ -75,14 +75,29 @@ class Player(GalagaSprite):
 
 
 class Enemy(GalagaSprite):
+    zapdos = os.path.join(os.path.expanduser('~'), 'Desktop', 'downloads', 'zapdossprite.png')
+    def __init__(self, screen, x, y, speed, radius, zapdos):
+        
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.radius = radius
+        self.angle = 0
+        self.image = pygame.image.load(zapdos)
+        self.image = pygame.transform.scale(self.image, (radius * 2, radius * 2))
 
-    FRAMES = {
-            'test': [(96, 32, 16, 16)],
-            }
+    def draw(self):
+        self.screen.blit(self.image, (self.x - self.radius, self.y - self.radius))
 
-    def __init__(self, x, y, enemy_type):
-        super(Enemy, self).__init__(x, y, 16, 16)
-        self.enemy_type = enemy_type
+    def move(self):
+        self.angle += self.speed
+        self.x = self.screen.get_width() / 2 + self.radius * math.cos(self.angle)
+        self.y = self.screen.get_height() / 2 + self.radius * math.sin(self.angle)
+
+    def update(self):
+        self.move()
+        self.draw()
 
     def get_frame (self):
         return 0
