4a9e2140c1617068726eb68525896bbc31bed4ab
sebmarl <149390512+sebmarl@users.noreply.github.com>
2023-11-28 15:16:16 -0500

    Update control_point.py

--- python-galaga-main/sprites/control_point.py
+++ python-galaga-main/sprites/control_point.py
@@ -47,6 +47,11 @@ used as the movement path for the insectoids SM """
             self.y = self.control_points.get_quartet(
                 self.q_index).get_point(self.p_index).y
             self.rect = self.image.get_rect(center=(self.x, self.y))
+""" Controls the points position, appearance and selected state. The function is the update method\
+for the ControlPoint class and function created above. It checks the mouse position, which button was pressed\
+Then it checks if the mouse cursor is over the control point, then if the cursor was over the \
+control point the image attribut gets set to selected image, otherwise it goes to original image.\ 
+Then if the control point is selected, the positions of the control point are updated. """ 
 
     def get_surf(self):
         return self.image
