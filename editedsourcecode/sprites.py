7bf792ce48d1b6bb08a04134316365eed5262dc9
sebmarl <149390512+sebmarl@users.noreply.github.com>
2023-11-28 12:58:35 -0500

    Update sprites.py

--- editedsourcecode/sprites.py
+++ editedsourcecode/sprites.py
@@ -112,7 +112,7 @@ class Missile(GalagaSprite):
         else:
             img_slice = self.PLAYER_MISSILE
         ix, iy, w, h = img_slice
-        self.image = grab_sheet(64 ,112 , 32, 32)
+        self.image = grab_sheet(246 ,67 , 3, 8)
 
     def update(self, delta_time: int, flash_flag: bool):
         vel = self.vel * delta_time
