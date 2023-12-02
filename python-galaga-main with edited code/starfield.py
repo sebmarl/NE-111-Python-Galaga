7b06415b509502c6f3801d7ff17c80e9b54cd367
sebmarl <149390512+sebmarl@users.noreply.github.com>
2023-12-02 10:24:44 -0500

    Update starfield.py

--- python-galaga-main with edited code/starfield.py
+++ python-galaga-main with edited code/starfield.py
@@ -1,4 +1,4 @@
-
+""" Sebastian Marion-Landais """ 
 import pygame
 import random
 import constants
@@ -12,7 +12,7 @@ class StarField():
     def __init__(self):
         self.star_field_slow = self.create_stars(50)
         self.star_field_medium = self.create_stars(35)
-        self.star_field_fast = self.create_stars(30)
+        self.star_field_fast = self.create_stars(30) """ Creates the initialize funtcion for the starfield, setting the # of stars that appear SM """ 
 
     def create_stars(self, number_of_stars):
         stars = []
@@ -20,7 +20,7 @@ class StarField():
             star_loc_x = random.randrange(0, constants.SCREEN_WIDTH)
             star_loc_y = random.randrange(0, constants.SCREEN_HEIGHT)
             stars.append([star_loc_x, star_loc_y])
-        return stars
+        return stars """ Sets the stars to cover the width of the screen in a random range using the random.randrange method SM """ 
 
     def render_stars(self, screen, star_collection, speed, size, color):
         for star in star_collection:
@@ -28,9 +28,10 @@ class StarField():
             if star[1] > constants.SCREEN_HEIGHT:
                 star[0] = random.randrange(0, constants.SCREEN_WIDTH)
                 star[1] = random.randrange(-20, -5)
-            pygame.draw.circle(screen, color, star, size)
+            pygame.draw.circle(screen, color, star, size) """ sets the star speed and range over the screen SM """ 
 
     def render(self, screen):
         self.render_stars(screen, self.star_field_slow, 1, 3, DARKGREY)
         self.render_stars(screen, self.star_field_medium, 4, 2, LIGHTGREY)
-        self.render_stars(screen, self.star_field_fast, 8, 1, YELLOW)
+        self.render_stars(screen, self.star_field_fast, 8, 1, YELLOW) 
+        """ sets the colour of the stars in game SM """ 
