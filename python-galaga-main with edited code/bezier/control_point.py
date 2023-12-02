4f0eee037d1ecae5fe078b6c9976ce86639ec924
advaitgore <150055243+advaitgore@users.noreply.github.com>
2023-12-02 18:05:32 -0500

    Update control_point.py

--- python-galaga-main with edited code/bezier/control_point.py
+++ python-galaga-main with edited code/bezier/control_point.py
@@ -1,4 +1,6 @@
+"""AG(Advait Gore)"""
+#this class represents a point in a 2D space
 class ControlPoint():
-    def __init__(self, x: float, y: float):
+    def __init__(self, x: float, y: float):#this takes two values from x and y, and sets the instance's x and y attributes to these values
         self.x = x
         self.y = y
