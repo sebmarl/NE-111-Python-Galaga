fca04cc0c0d59a3a612fdcfb0daca70373a333bd
advaitgore <150055243+advaitgore@users.noreply.github.com>
2023-12-02 19:15:48 -0500

    Update control_point_handler.py

--- python-galaga-main/bezier/control_point_handler.py
+++ python-galaga-main/bezier/control_point_handler.py
@@ -1,4 +1,7 @@
-﻿class ControlPointHandler():
+"""AG(Advait Gore)"""
+#This class creates the control point handler
+#It is used to track the control point that is being manipulated
+class ControlPointHandler():
     def __init__(self, quartet_index, control_point_index):
         self.quartet_index = quartet_index
         self.control_point_index = control_point_index
