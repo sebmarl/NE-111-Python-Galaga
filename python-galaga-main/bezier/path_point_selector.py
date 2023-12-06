4f9a0df240b69847f8a72ef92c321c7c9c60bf6d
sebmarl <149390512+sebmarl@users.noreply.github.com>
2023-12-05 22:14:03 -0500

    Update path_point_selector.py

--- python-galaga-main/bezier/path_point_selector.py
+++ python-galaga-main/bezier/path_point_selector.py
@@ -14,8 +14,8 @@ class PathPointSelector():
         if control_point_handler.control_point_index == 0 or control_point_handler.control_point_index == 3:
             return True
         return False
-        """ Creates the intialize function that works with quartets in order to determine the path of control points for the enemies class\
-        using the is path point method, it checks if the control point is part of the path. SM """ 
+        #Creates the intialize function that works with quartets in order to determine the path of control points for the enemies class\
+        #using the is path point method, it checks if the control point is part of the path. SM """ 
 
     def create_path_point_mapping(self):
 
@@ -36,8 +36,8 @@ class PathPointSelector():
 
             self.path_point_mapping[self.create_key(index, 0)] = ControlPointHandler(mapped_first_quartet_index, 3)
             self.path_point_mapping[self.create_key(index, 3)] = ControlPointHandler(mapped_last_quartet_index, 0)
-            """ The path point mapping function creates mappings between the first and last control points of the quartet. These values are then\
-            stored in the path point mapping method and then turned into keys for future reference SM """ 
+            #The path point mapping function creates mappings between the first and last control points of the quartet. These values are then\
+            #stored in the path point mapping method and then turned into keys for future reference SM """ 
 
     
     def find_related_path_point(self, control_point_handler: ControlPointHandler):
@@ -47,8 +47,8 @@ class PathPointSelector():
         else:
             print('error')
             exit(1)
-            """ checks if something is a path point, if so it hten creates a key and looks up the valid path point in\
-            path point mapping method. Otherwise an error is printed if it is not a path point SM """ 
+        #checks if something is a path point, if so it hten creates a key and looks up the valid path point in\
+            #path point mapping method. Otherwise an error is printed if it is not a path point SM """ 
 
     def find_related_control_point(self, control_point_handler: ControlPointHandler):
         related_control_point = ControlPointHandler(-1, -1)
@@ -68,9 +68,9 @@ class PathPointSelector():
             else:
                 related_control_point.quartet_index = 0
         return related_control_point
-        """ This function checks the related control point based on the ControlPointHandler \
-                method and the it is set to specific conditions based on the original points index and\
-                quartet index. SM """ 
+        #This function checks the related control point based on the ControlPointHandler \
+                #method and the it is set to specific conditions based on the original points index and\
+                #quartet index. SM """ 
 
     def get_last_quartet_index(self):
         return self.control_point_quartet_collection.number_of_quartets() - 1
@@ -91,9 +91,9 @@ class PathPointSelector():
         related_control_point.quartet_index = control_point_handler.quartet_index
 
         return related_control_point
-        """ Determines the control point solely on the control point index. If the quartet index\
-        is related ot the control point, it is then set to the same quartet index as the original control\
-        point. SM """ 
+    #Determines the control point solely on the control point index. If the quartet index\
+        #is related ot the control point, it is then set to the same quartet index as the original control\
+        #point. SM """ 
 
     def find_control_points_of_path_point(self, path_point_handler: ControlPointHandler):
         related_control_points = []
@@ -121,9 +121,9 @@ class PathPointSelector():
             exit(1)
 
         return related_control_points
-        """ This function determines the related control points based on their path. The related\
-        points are then added to a list which gets returned. IF there are any invalid point indexes in the\ 
-        list, an error is returned and the game exits with error 1 SM """ 
+        #This function determines the related control points based on their path. The related\
+        #points are then added to a list which gets returned. IF there are any invalid point indexes in the\ 
+        #list, an error is returned and the game exits with error 1 SM """ 
 
     def get_control_point_pairs(self):
         line_list = []
